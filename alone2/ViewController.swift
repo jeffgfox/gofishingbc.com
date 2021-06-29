bbca843c31652de4d2c5c993621a1183de467d06
jeffgfox <82912173+jeffgfox@users.noreply.github.com>
2021-06-29 00:29:47 -0700

    Initial Commit

---
+++ alone2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  alone2
+//
+//  Created by Jeff Goldman on 2021-06-29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
